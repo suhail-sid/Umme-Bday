097d2a21bc181c9f12f5b1450aa2c217e3e04f08
Suhail Siddiqui <suhailsiddiqui1530@gmail.com>
2026-05-29 17:44:08 +0530

    made changex

--- src/components/VideoMessageScreen.jsx
+++ src/components/VideoMessageScreen.jsx
@@ -19,7 +19,6 @@ export default function VideoMessageScreen({ isActive, onNext }) {
       }
     }
   };
-
   return (
     <ScreenWrapper isActive={isActive} className="overflow-y-auto hide-scrollbar">
 
